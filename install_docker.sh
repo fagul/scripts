b24e1b415ab9d887ea2d66c08fc3bfc9a269eff3
PreetiCK <preetikhodanpur038@gmail.com>
2015-05-27 18:21:50 +0530

    adding docker_install.sh file

---
+++ install_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get -y update
+
+# install Apache2
+#apt-get -y install apache2 
+
+# write some HTML
+#echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
+
+# restart Apache
+#apachectl restart
+
+#Install Docker
+wget -qO- https://get.docker.com/ | sh
