312c157f06976d3b13a6bc67095fa74c70bd8457
PreetiCK <preetikhodanpur038@gmail.com>
2015-05-29 17:37:34 +0530

    ss

--- install_docker.sh
+++ install_docker.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 apt-get -y update
 
-#Install docker
-apt-get -y docker.io
- 
-source /etc/bash_completion.d/docker.io
-
 #Run Docker
 docker run --name hello -d -p 8080:8080 preetick/mthello:1.1
