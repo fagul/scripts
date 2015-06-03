00e8e8b53b5b2ba775547785e15d36751e02ea11
PreetiCK <preetikhodanpur038@gmail.com>
2015-06-03 12:34:27 +0530

    df

--- vehiclefe.sh
+++ vehiclefe.sh
@@ -2,4 +2,4 @@
 apt-get update
 wget -qO- https://get.docker.com/ | sh
 #Run Docker
-docker run --name hello -d -p 8083:8080 preetick/mt_presentation:1.0
+docker run --name webp -d -p 8083:8080 preetick/mt_presentation:1.0
