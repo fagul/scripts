6776a6774226be098ee5b0ca67d841f7d4309496
PreetiCK <preetikhodanpur038@gmail.com>
2015-06-07 17:51:20 +0530

    changed region to southasia

--- business_conf.sh
+++ business_conf.sh
@@ -2,4 +2,4 @@
 apt-get update
 wget -qO- https://get.docker.com/ | sh
 #Run Docker
-docker run --name vapp -d -p 8080:8080 -e DB_IP=dockerjenkinserver.cloudapp.net -e DB_PASSWORD=root -e DB_PORT=3306 -e DB=VehicleRental preetick/business:2.1.4
+docker run --name vapp -d -p 8080:8080 -e DB_IP=dbvminstance.southeastasia.cloudapp.azure.com -e DB_PASSWORD=root -e DB_PORT=3306 -e DB=VehicleRental preetick/business:2.1.4
