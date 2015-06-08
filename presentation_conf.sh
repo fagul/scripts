216b3ae098a43fe7f25294b491914d24d98dee5d
PreetiCK <preetikhodanpur038@gmail.com>
2015-06-08 14:34:33 +0530

    dfsg

--- presentation_conf.sh
+++ presentation_conf.sh
@@ -2,4 +2,4 @@
 apt-get update
 wget -qO- https://get.docker.com/ | sh
 #Run Docker
-docker run --name webp -d -p 8080:8080 -e BUSINESS_IP=businessvminstance.southeastasia.cloudapp.azure.com preetick/mt_presentation:1.0
+docker run --name webp -d -p 8080:8080 -e BUSINESS_IP=businessvminstance.westus.cloudapp.azure.com preetick/mt_presentation:1.0
