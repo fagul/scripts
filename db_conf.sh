5d3df195f51944ac27d1bac32e1bde30cbdcd1bf
PreetiCK <preetikhodanpur038@gmail.com>
2015-06-08 18:15:24 +0530

    hgfh

--- db_conf.sh
+++ db_conf.sh
@@ -2,4 +2,4 @@
 apt-get update
 wget -qO- https://get.docker.com/ | sh
 #Run Docker
-docker run --name dbmysql -d -p 3306:3306 -v /data/mysql:/var/lib/mysql preetick/mysqlimage:v3
+docker run --name dbmysql -d -p 3306:3306 -v /data/mysql:/var/lib/mysql preetick/dbimage:v1
