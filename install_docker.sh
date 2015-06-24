53d6d9ffd156d23d68e656ae955f812405169bea
fagul <fagul.pandey@mindtree.com>
2015-06-24 11:27:24 +0530

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,4 +2,4 @@
 apt-get update
 wget -qO- https://get.docker.com/ | sh
 #Run Docker
-docker run --name hello -d -p 8080:8080 preetick/mthello:1.1
+docker run --name hello -d -p 8080:8080 fagul/mthello:1.0
