415388cef56ff0ba7167f5b86814dcc10e17e208
fagul <fagul.pandey@mindtree.com>
2015-06-30 15:40:11 +0530

    Update presentation_conf.sh

--- presentation_conf.sh
+++ presentation_conf.sh
@@ -2,4 +2,4 @@
 apt-get update
 wget -qO- https://get.docker.com/ | sh
 #Run Docker
-docker run --name webp -d -p 8080:8080 -e BUSINESS_IP=businessvminstance.southeastasia.cloudapp.azure.com fagul/mt_presentation:3.1.2
+docker run --name webp -d -p 8080:8080 -e BUSINESS_IP=businessvminstance.westus.cloudapp.azure.com fagul/mt_presentation:3.1.4
