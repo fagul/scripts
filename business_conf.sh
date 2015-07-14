9b9a8b46228dc5f77456213ec3439ebd5753effa
fagul <fagul.pandey@mindtree.com>
2015-07-14 14:34:05 +0530

    Update business_conf.sh

--- business_conf.sh
+++ business_conf.sh
@@ -2,4 +2,4 @@
 apt-get update
 wget -qO- https://get.docker.com/ | sh
 #Run Docker
-docker run --name vapp -d -p 8080:8080 -e DB_IP=dbvminstance.westus.cloudapp.azure.com -e DB_PASSWORD=welcome123 -e DB_PORT=3306 -e DB=VehicleRental nijisha/mt-vehicle-rental-bussiness
+docker run --name vapp -d -p 8080:8080 -e DB_IP=dbvminstance.westus.cloudapp.azure.com -e DB_PASSWORD=welcome123 -e DB_PORT=3306 -e DB=VehicleRental nijisha/mt-vr-business
