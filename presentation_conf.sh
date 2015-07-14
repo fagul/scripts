21eb4995781a4ee2c0fc2ff2cf1bca5259365607
fagul <fagul.pandey@mindtree.com>
2015-07-14 14:35:25 +0530

    Update presentation_conf.sh

--- presentation_conf.sh
+++ presentation_conf.sh
@@ -2,4 +2,4 @@
 apt-get update
 wget -qO- https://get.docker.com/ | sh
 #Run Docker
-docker run --name webp -d -p 8080:8080 -e BUSINESS_IP=businessvminstance.westus.cloudapp.azure.com nijisha/mt-vehicle-rental-presentaion
+docker run --name webp -d -p 8080:8080 -e BUSINESS_IP=businessvminstance.westus.cloudapp.azure.com nijisha/mt-mvn-vr-presentation
